4e2976ba60554d20ed352db786196ba845f92873
seboebinger <59028115+seboebinger@users.noreply.github.com>
2024-02-27 12:57:40 -0500

    Removed redundant functions from assembleTwoChannels

--- assembleTwoChannels.m
+++ assembleTwoChannels.m
@@ -5,13 +5,6 @@ out2 = channelDelay(threshold(signals2'*gains2'),delay2,atime);
 
 out = threshold(out1+out2);
 
-	function out = channelDelay(in,delay,atime)
-		out = interp1(atime,in,atime-delay,'linear',0);
-	end
-
-	function out = threshold(in)
-		out = max(in,0);
-	end
 
 end
 
